a84dc820306d7a8c30687abcb1e86a69fed34149
trisha-tomy <122303927+trisha-tomy@users.noreply.github.com>
2023-01-20 01:07:46 +0530

    Submitting Q8 of Assignment1

---
+++ a1_q8.java
@@ -0,0 +1,28 @@
+import java.util.*;
+    
+    public class a1_q8{
+    
+    public static void main(String[] args) {
+        Scanner scn= new Scanner(System.in);
+        int num1,num2,temp1,temp2,temp;
+        num1= scn.nextInt();
+        num2= scn.nextInt();
+        temp1=num1;
+        temp2=num2;
+
+
+        while(temp2 != 0){
+           temp = temp2;
+          
+           temp2 = temp1%temp2;
+    
+           temp1 = temp;
+   
+      }
+
+      int hcf = temp1;
+      System.out.println(hcf);
+      int lcm = (num1*num2)/hcf;
+      System.out.println(lcm);
+     }
+    }
