870e56fad0e1e0d0d55ee0a600b6c366d3b8f9bb
trisha-tomy <122303927+trisha-tomy@users.noreply.github.com>
2023-01-19 19:02:55 +0530

    Submitting Q1 of Assignment1

---
+++ a1_q1.java
@@ -0,0 +1,11 @@
+public class a1_q1 {
+
+  public static void main(String[] args) {
+    System.out.println("*****");
+    System.out.println("   * ");
+    System.out.println("  *  ");
+    System.out.println(" *   ");
+    System.out.println("*****");
+
+  }
+}
