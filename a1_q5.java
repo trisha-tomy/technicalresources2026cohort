d902618c0b211c156a2dc462e285f7542356da31
trisha-tomy <122303927+trisha-tomy@users.noreply.github.com>
2023-01-19 20:50:20 +0530

    Submitting Q5 of Assignment1

---
+++ a1_q5.java
@@ -0,0 +1,22 @@
+import java.util.*;
+  
+  public class a1_q5{
+  
+  public static void main(String[] args) {
+      Scanner scn= new Scanner(System.in);
+      int n= scn.nextInt();
+      int count=0;
+      int num=n;
+      while (num>0){
+          int rem= num%10;
+          if (rem>0){
+              count++;
+              num=num/10;
+          }
+          else{
+              continue;
+          }
+      }
+      System.out.println(count);
+   }
+  }
