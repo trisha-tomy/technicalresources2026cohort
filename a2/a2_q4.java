c1cbe2f4d7aed1877b8ce4d684709650b0c6068a
trisha-tomy <122303927+trisha-tomy@users.noreply.github.com>
2023-01-24 20:18:58 +0530

    Submitting Q4 of Assignment2

---
+++ a2/a2_q4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class a2_q4{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        pattern(n);
+
+    }
+
+    static void pattern(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("   ");
+            }
+            for (int col = n; col >= row; col--) {
+                System.out.print(" * ");
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+}
