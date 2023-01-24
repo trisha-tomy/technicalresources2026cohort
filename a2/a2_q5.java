9e30d34c4c821635ae8f302ed35f26a2f2032ee4
trisha-tomy <122303927+trisha-tomy@users.noreply.github.com>
2023-01-24 19:25:51 +0530

    Submitting Q5 of Assignment2

---
+++ a2/a2_q5.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class a2_q5 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n= scn.nextInt();
+        pattern(n);
+
+        }
+    static void pattern(int n){
+        for (int row = 1; row <=2*n-1 ; row++) {
+            int total_col=row>n?2*n-row:row;
+            int spaces=n-total_col;
+            for (int space = 0; space < spaces; space++) {
+                System.out.print("   ");
+            }
+
+            for (int col = 1; col <=total_col ; col++) {
+                System.out.print("*     ");
+
+            }
+            System.out.println();
+
+        }
+    }
+}
+
