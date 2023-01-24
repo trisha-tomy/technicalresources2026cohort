27086cb2252e853cd52146e92bb7b6764c2a50c0
trisha-tomy <122303927+trisha-tomy@users.noreply.github.com>
2023-01-24 20:36:07 +0530

    Submitting Q11 of Assignment2

---
+++ a2/a2_q11.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class a2_q11 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        pattern(n);
+
+    }
+
+    static void pattern(int n) {
+        int i=1;
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(i+" ");
+                i+=1;
+            }
+            System.out.println();
+        }
+    }
+}
