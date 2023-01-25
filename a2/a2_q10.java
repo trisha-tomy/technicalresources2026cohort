ea082e526426163e0dfda129a92804e855290dc2
trisha-tomy <122303927+trisha-tomy@users.noreply.github.com>
2023-01-25 21:09:10 +0530

    Submitting Q10 of Assignment2

---
+++ a2/a2_q10.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class a2_q10 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        pattern(n);
+
+    }
+    static void pattern(int n) {
+        for (int row = 1; row <= 2 * n - 1; row++) {
+            if (row % 2 != 0) {
+                int total_col = row > n ? 2 * n - row : row;
+                int spaces = n - total_col;
+                for (int space = 1; space <=spaces; space++) {
+                    System.out.print("   ");
+                }
+                System.out.print(" * ");
+
+                while (row!=1&&row!=2*n-1) {
+                    for (int col = 1; col <= total_col+(total_col-3); col++) {
+                        System.out.print("   ");
+
+                    }
+                    System.out.print(" * ");
+                    break;
+
+
+                }
+
+                /*for (row = 3; row <2*n-1 ; row++) {
+                    System.out.print("* ");
+                }*/
+
+                System.out.println();
+
+            }
+        }
+    }
+
+
+}
