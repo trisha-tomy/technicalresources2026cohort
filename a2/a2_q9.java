33946925187c01f93eb7139cf610f5d3b6f3a2ac
trisha-tomy <122303927+trisha-tomy@users.noreply.github.com>
2023-01-25 23:23:21 +0530

    Submitting Q9 of Assignment2

---
+++ a2/a2_q9.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class pattern3 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        pattern(n);
+
+    }
+    static void pattern(int n) {
+        for (int row = 1; row <= 2 * n - 1; row++) {
+            if (row % 2 != 0) {
+                int total_col = row > n ? 2 * n - row : row;
+                
+                for (int col = 1; col < total_col; col++) {
+                    System.out.print("   ");
+
+                }
+
+                System.out.print(" * ");
+
+                while (row!=n) {
+                    for (int col = 2*(n-total_col)-1; col >=1; col--) {
+                        System.out.print("   ");
+                    }
+                    System.out.print(" * ");
+                    break;
+
+                }
+                
+                System.out.println();
+
+            }
+        }
+    }
+
+
+}
